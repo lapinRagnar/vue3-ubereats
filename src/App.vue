f1e5e49ba519cf30b834c02bfd445bef8ad4b778
lapinRagnar <lapinragnar@gmail.com>
2022-07-03 10:45:27 +0200

    ajout component RestaurantCard

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,31 @@
 <template>
   <div id="app">
-
+    <RestaurantCard/>
   </div>
 </template>
 
 <script>
 
+  import RestaurantCard from './components/RestaurantCard.vue'
 
-export default {
-  name: 'App',
- 
-}
+  export default {
+    
+    name: 'App',
+    components: {
+      RestaurantCard
+    }
+  }
 </script>
 
 <style>
-@import url('./style/reset.css');
-@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,400;0,700;0,900;1,100;1,400;1,900&display=swap');
-#app {
-  font-family: Roboto, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import url('./style/reset.css');
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,400;0,700;0,900;1,100;1,400;1,900&display=swap');
+  #app {
+    font-family: Roboto, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/RestaurantCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="restaurant--card">
+    <p>test</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" >
+    .restaurant--card{
+        width: 32%;
+        height: 30vh;
+        p{
+            margin: 0px;
+        }
+    }
+</style>
