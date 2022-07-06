ed11b2841372d891b67125e6eb2702232067b9e5
lapinRagnar <lapinragnar@gmail.com>
2022-07-06 10:27:30 +0200

    ajout dropdown sur la input de recherche - v-for watch()

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,7 +2,17 @@
   <div class="home">
     <div class="header">
         <img src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/ee037401cb5d31b23cf780808ee4ec1f.svg" alt="">
-        <input v-model="user_search_restaurant" type="text" placeholder="de quoi avez-vous envie?">
+        <div class="wrapper--input">
+            <input v-model="user_search_restaurant" type="text" placeholder="de quoi avez-vous envie?">
+            <div class="search">
+                <div v-for="(restaurant, i) in search_restaurant" :key="i" class="container--restaurant--search">
+                    <div class="wrapper--img">
+                        <img :src="restaurant.image" alt="">
+                    </div>
+                    <p>{{restaurant.name}}</p>
+                </div>
+            </div>
+        </div>
     </div>
     <div class="banier"></div>
     <RestaurantRow v-for="(data, index) in dataRestaurant" :key="index" :three_restaurant="data" />
@@ -55,17 +65,19 @@ export default {
         // user search restaurants
 
         let user_search_restaurant = ref('')
+        let search_restaurant = ref([])
+
         watch(user_search_restaurant, (new_value) => {
-            let regex =RegExp(new_value)
-            let search_restaurant = all_restaurant.filter(restaurant => regex.test(restaurant.name))
-            console.log(search_restaurant);
+            let regex =RegExp(new_value.toLowerCase())
+            let new_search_restaurant = all_restaurant.filter(restaurant => regex.test(restaurant.name.toLowerCase()))
+            new_value == 0 ? search_restaurant.value = [] : search_restaurant.value = new_search_restaurant
        })
 
         // makeDataReastaurant();
         onMounted(makeDataReastaurant)
 
         return {
-            dataRestaurant, user_search_restaurant
+            dataRestaurant, user_search_restaurant, search_restaurant
         }
     }
 }
@@ -82,7 +94,11 @@ export default {
         img{
           width: 200px;
         }
-        input{
+
+        .wrapper--input{
+          position: relative;
+
+          input{
           background: #f6f6f6;
           border: none;
           width: 400px;
@@ -90,7 +106,42 @@ export default {
           border-radius: 5px;
           outline: none;
           padding-left: 20px;
+          }
+
+          .search{
+            position: absolute;
+            top: 100%;
+            width: 100%;
+            background-color: #fff;
+            border-radius: 5px;
+            border: 1px solid rgba(172, 255, 47, 0.775);
+
+            .container--restaurant--search{
+              display: flex;
+              align-items: center;
+              padding: 10px;
+
+              &:hover{
+                background-color: #f6f6f6;
+              }
+
+              .wrapper--img{
+                height: 40px;
+                width: 40px;
+                margin-right: 30px;
+                border-radius: 50%;
+                overflow: hidden;
+
+                img{
+                  height: 100%;
+                  width: auto;
+                }
+              }
+              
+            }
+          }
         }
+
     }
     .banier{
       height: 200px;
