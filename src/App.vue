4a9ba42fec037d5e4633cab388b73570fbb1819e
lapinRagnar <lapinragnar@gmail.com>
2022-07-06 08:01:44 +0200

    ajout header

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,6 @@ import HomePage from './pages/home.vue'
   @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,400;0,700;0,900;1,100;1,400;1,900&display=swap');
   #app {
     font-family: Roboto, sans-serif;
-    padding: 20px;
+    padding: 0 20px;
   }
 </style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="home">
+    <div class="header">
+        <img src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/ee037401cb5d31b23cf780808ee4ec1f.svg" alt="">
+        <input type="text" placeholder="de quoi avez-vous envie?">
+    </div>
+    <div class="banier"></div>
     <RestaurantRow v-for="(data, index) in dataRestaurant" :key="index" :three_restaurant="data" />
   </div>
 </template>
@@ -54,6 +59,33 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss">
+  .home{
+    .header{
+      height: 120px;
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+        img{
+          width: 200px;
+        }
+        input{
+          background: #f6f6f6;
+          border: none;
+          width: 400px;
+          height: 60px;
+          border-radius: 5px;
+          outline: none;
+          padding-left: 20px;
+        }
+    }
+    .banier{
+      height: 200px;
+      width: 100%;
+      background-image: url("https://www.ubereats.com/restaurant/_static/7b308f7cbbf8e335ceda0447a8bd7c63.png");
+      background-size: cover;
+      background-position: center center;
+    }
+  }
 </style>
